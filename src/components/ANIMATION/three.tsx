d9baf23be21a38f1c0c643c9f3f8a4cddb0da795
priya shinde <priya.shinde9390@gmail.com>
2024-08-25 17:53:47 +0530

    f

--- src/components/ANIMATION/three.tsx
+++ src/components/ANIMATION/three.tsx
@@ -3,18 +3,18 @@ import Image from 'next/image'
 
 const Three: React.FC = () => {
     return (
-        <div className="relative h-[100vh] bg-black overscroll-x-none overflow-x-hidden">
+        <div className="relative h-[100vh] bg-white overscroll-x-none overflow-x-hidden">
           <motion.div 
           animate={{
-            x: [-1400, -1200, -1000, -800, -600, -400, -200, 0, 200, 400, 600, 800, 1000, 1200, 1400],
-            y: [0, 30, 0, 30, 0, 30, 0, 30, 0, 30, 0, 30, 0, 30, 0]
+            x: [-1500,-1400, -1200, -1000, -800, -600, -400, -200, 0, 200, 400, 600, 800, 1000, 1200, 1400],
+            y: [20, 0, 30, 0, 35, -10, 30, 0, 35, 20, 25, 10, 0, 15, 30, 35]
           }}
           transition={{
               duration: 6,
               ease: 'linear',
               repeat: Infinity
           }}
-          className="relative flex flex-row place-content-between w-full h-[35vh]">
+          className="absolute flex flex-row place-content-between w-full h-[35vh]">
 
               <Image 
               alt="img" 
@@ -24,9 +24,48 @@ const Three: React.FC = () => {
               className="relative h-full"/>
               <Image 
               alt="img" 
-              src="/images/glow.png" 
-              width={250}
-              height={250}
+              src="/images/4c.png" 
+              width={180}
+              height={200}
+              className="relative h-full"/>
+              <Image 
+              alt="img" 
+              src="/images/2c.png" 
+              width={180}
+              height={200}
+              className="relative h-full"/>
+              <Image 
+              alt="img" 
+              src="/images/3c.png" 
+              width={180}
+              height={200}
+              className="relative h-full"/>
+
+          </motion.div>
+
+          <motion.div 
+          animate={{
+            x: [-1400, -1200, -1000, -800, -600, -400, -200, 0, 200, 400, 600, 800, 1000, 1200, 1400],
+            y: [10, 30, 35, 30, 20, 20, 0, 20, -10, 30, 35, 30, 0, 30, 0]
+          }}
+          transition={{
+              duration: 6,
+              ease: 'linear',
+              repeat: Infinity
+          }}
+          className="absolute top-0 flex flex-row place-content-between w-full h-[35vh]">
+
+              <Image 
+              alt="img" 
+              src="/images/1c.png" 
+              width={180}
+              height={200}
+              className="relative h-full"/>
+              <Image 
+              alt="img" 
+              src="/images/4c.png" 
+              width={180}
+              height={200}
               className="relative h-full"/>
               <Image 
               alt="img" 
