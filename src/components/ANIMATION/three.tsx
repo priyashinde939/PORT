8c0980de970b4bdc09ba155aba008c3bb3f35029
priya shinde <priya.shinde9390@gmail.com>
2024-08-26 23:33:03 +0530

    g

--- src/components/ANIMATION/three.tsx
+++ src/components/ANIMATION/three.tsx
@@ -4,6 +4,12 @@ import Image from 'next/image'
 const Three: React.FC = () => {
     return (
         <div className="relative h-[100vh] bg-white overscroll-x-none overflow-hidden rounded-[4rem] inset-0">
+
+          <div className="absolute top-28 h-full w-[70vw] text-black text-left text-8xl font-extrabold flex flex-col gap-12">
+              <p>I help you</p>
+              <p>shine in a crowded </p>
+              <p>market.</p>
+          </div>
  
           <div className='relative top-20'>
 
@@ -163,7 +169,7 @@ const Three: React.FC = () => {
             y: [0, 20, 0, 30, -10, 30, 0, 30, -10, 30, 35, 0, 30, 10, 30]
           }}
           transition={{
-              duration: 9,
+              duration: 10,
               ease: 'linear',
               repeat: Infinity,
               repeatDelay:2
@@ -227,7 +233,7 @@ const Three: React.FC = () => {
             y: [0, 30, 15, 30, 0, 20, 0, 20, -10, 30, 35, 10, 0, 30, 0]
           }}
           transition={{
-              duration: 7,
+              duration: 10,
               ease: 'linear',
               repeat: Infinity
           }}
@@ -240,7 +246,9 @@ const Three: React.FC = () => {
               height={200}
               className="relative h-full mx-10"/>
 
-          </motion.div>  
+          </motion.div> 
+
+          // Extra // 
 
           <motion.div 
           // reversed
@@ -277,6 +285,75 @@ const Three: React.FC = () => {
               height={200}
               />
           </motion.div>
+
+          <motion.div 
+          // ---------> 3 //
+          animate={{
+            x: [-2000,-1900,-1800, -1700, -1600,-1500,-1400, -1200, -1000, -800, -600, -400, -200, 0, 200, 400, 600, 800, 1000, 1200, 1400],
+            y: [0, 40, 0, 30, -20, 30, 0, -20, 0, 40, -10, 30, 0, 30, 20, 25, 10, 0, 15, 30, 35]
+          }}
+          transition={{
+              duration: 12,
+              ease: 'linear',
+              repeat: Infinity,
+              delay:3
+              
+          }}
+          className="absolute flex flex-row place-content-around top-96 w-full h-[35vh]">
+
+              <Image 
+              alt="img" 
+              src="/images/8c.png" 
+              width={180}
+              height={200}
+
+              />
+              <Image 
+              alt="img" 
+              src="/images/2c.png" 
+              width={180}
+              height={200}
+              className='mx-60'
+
+              />
+              <Image 
+              alt="img" 
+              src="/images/7c.png" 
+              width={180}
+              height={200}
+              />
+          </motion.div>
+
+          <motion.div 
+          // <---------- 2
+          animate={{
+            x: [1400, 1200, 1000, 800, 600, 400, 200, 0, -200, -400, -600, -800, -1000, -1200, -1400, -1450],
+            y: [0, 20, 0, 30, -10, 30, 0, 30, -10, 30, 35, 0, 30, 10, 30]
+          }}
+          transition={{
+              duration: 10,
+              ease: 'linear',
+              repeat: Infinity,
+              delay:2,
+              repeatDelay:3
+          }}
+          className="absolute flex flex-row place-content-evenly top-96 w-full h-[35vh] ">
+              <Image 
+              alt="img" 
+              src="/images/9c_flipped.png" 
+              width={180}
+              height={200}
+              className="relative h-full mr-40"/>
+
+              <Image 
+              alt="img" 
+              src="/images/2c_flipped.png" 
+              width={180}
+              height={200}
+              className="relative h-full ml-40"/>
+
+          </motion.div>  
+
 
           </div>
 
