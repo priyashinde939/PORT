52cf45074ac5ed1c81811b47a49b00e502dd66b3
priya shinde <priya.shinde9390@gmail.com>
2024-08-30 18:39:42 +0530

    c

--- src/components/Section3D/Section3D2.tsx
+++ src/components/Section3D/Section3D2.tsx
@@ -10,7 +10,7 @@ const Section3D2: React.FC = () => {
     
             <motion.div
             className={`relative basis-2/3 border border-gray-800 overflow-hidden rounded-full shadow-lg flex flex-row min-w-[150px] text-red-700 items-center justify-center text-9xl font-black`}
-            initial={{ transform: "matrix3d(1, 0, 0.5, 0,   0, 1, 0, 0.03,   0.2, 0.1, 1, 0,   0, 0, 0, 1)" }}
+            initial={{ transform: "matrix3d(1, 0, 0.5, 0.001,   0, 1, 0.2, 0.01,   0.2, 0, 1, 0,   0, 0, 0, 1)" }}
             whileHover={{ transform: "matrix3d(1, 0, 0, 0,   0, 1, 0, 0,   0, 0, 1, 0,   0, 0, 0, 1)"  }}
             transition={{ type: 'linear', stiffness: 30, duration:0.8 }}
             style={{ flexGrow: 'FlexGrow', padding: '2rem', transformOrigin:'top-left', perspectiveOrigin:'center'  }}
