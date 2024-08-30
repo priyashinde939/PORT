11b2dd54dc5a4134bc1f79a4496e3d3af4438fa4
priya shinde <priya.shinde9390@gmail.com>
2024-08-30 20:55:48 +0530

    sgvcuahci jhcic

--- src/components/Section3D/Section3D2.tsx
+++ src/components/Section3D/Section3D2.tsx
@@ -11,10 +11,11 @@ const Section3D2: React.FC = () => {
             <motion.div
             className={`relative  overflow-hidden rounded-full w-fit left-[35%] text-red-700 items-center justify-center text-9xl font-black`}
             initial={{ transform: "matrix3d(1, 0, 0, 0.003,   0, 1, 0, 0,   0.2, 0, 1, 0,   0, 0, 0, 1)" }}
-            whileHover={{ transform: "matrix3d(1, 0, 0, -0.002,   0, 1, 0, 0,   0, 0, 1, 0,   0, 0, 0, 1)"  }}
+            whileHover={{ transform: "matrix3d(1, 0, 0, -0.001,   0, 1, 0, 0,   0, 0, 1, 0,   0, 0, 0, 1)"  }}
             transition={{ type: 'linear', stiffness: 30, duration:0.8 }}
             style={{ flexGrow: 'FlexGrow', padding: '2rem', transformOrigin:'top-left', perspectiveOrigin:'center'  }}
             >
+                
     
                 <h1>SLINK</h1>
                         
