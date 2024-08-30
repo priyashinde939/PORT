a9da931bc7052ff84d5d9bb3186bbd77287069a8
priya shinde <priya.shinde9390@gmail.com>
2024-08-30 19:48:40 +0530

    d

--- src/components/Section3D/Section3D2.tsx
+++ src/components/Section3D/Section3D2.tsx
@@ -6,12 +6,12 @@ const Section3D2: React.FC = () => {
     return (
         <div 
         style={{perspective: '500px', transformStyle:'preserve-3d'}}
-        className="flex flex-row p-6 gap-4 mx-10 min-h-[60vh] ">
-    
+        className="relative mx-10 min-h-[60vh] ">
+    {/* border border-gray-800 */}
             <motion.div
-            className={`relative basis-2/3 border border-gray-800 overflow-hidden rounded-full shadow-lg flex flex-row min-w-[150px] text-red-700 items-center justify-center text-9xl font-black overflow-`}
-            initial={{ transform: "matrix3d(1, 0, 0.5, 0.001,   0, 1, 0.2, 0.01,   0.2, 0, 1, 0,   0, 0, 0, 1)" }}
-            whileHover={{ transform: "matrix3d(1, 0, 0, 0,   0, 1, 0, 0,   0, 0, 1, 0,   0, 0, 0, 1)"  }}
+            className={`relative  overflow-hidden rounded-full w-fit left-[35%] text-red-700 items-center justify-center text-9xl font-black`}
+            initial={{ transform: "matrix3d(1, 0, 0, 0.003,   0, 1, 0, 0,   0.2, 0, 1, 0,   0, 0, 0, 1)" }}
+            whileHover={{ transform: "matrix3d(1, 0, 0, -0.002,   0, 1, 0, 0,   0, 0, 1, 0,   0, 0, 0, 1)"  }}
             transition={{ type: 'linear', stiffness: 30, duration:0.8 }}
             style={{ flexGrow: 'FlexGrow', padding: '2rem', transformOrigin:'top-left', perspectiveOrigin:'center'  }}
             >
