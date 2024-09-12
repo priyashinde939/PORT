889ebf5271478c280d4535166991d3031d693207
priya shinde <priya.shinde9390@gmail.com>
2024-09-12 13:33:29 +0530

    e

--- src/components/StackCard/Stack.tsx
+++ src/components/StackCard/Stack.tsx
@@ -19,35 +19,35 @@ const projects: Project[] = [
   {
     title: "Matthias Leidinger",
     description: "Originally hailing from Austria, Berlin-based photographer Matthias Leindinger is a young creative brimming with talent and ideas.",
-    src: "rock.jpg",
+    src: "1.jpg",
     link: "https://www.ignant.com/2023/03/25/ad2186-matthias-leidingers-photographic-exploration-of-awe-and-wonder/",
     color: "#BBACAF"
   },
   {
     title: "Clément Chapillon",
     description: "This is a story on the border between reality and imaginary, about the contradictory feelings that the insularity of a rocky, arid, and wild territory provokes.",
-    src: "tree.jpg",
+    src: "6.jpg",
     link: "https://www.ignant.com/2022/09/30/clement-chapillon-questions-geographical-and-mental-isolation-with-les-rochers-fauves/",
     color: "#977F6D"
   },
   {
     title: "Zissou",
     description: "Though he views photography as a medium for storytelling, Zissou’s images don’t insist on a narrative. Both crisp and ethereal, they’re encoded with an ambiguity—a certain tension—that lets the viewer find their own story within them.",
-    src: "water.jpg",
+    src: "2.jpg",
     link: "https://www.ignant.com/2023/10/28/capturing-balis-many-faces-zissou-documents-the-sacred-and-the-mundane-of-a-fragile-island/",
     color: "#C2491D"
   },
   {
     title: "Mathias Svold and Ulrik Hasemann",
     description: "The coastlines of Denmark are documented in tonal colors in a pensive new series by Danish photographers Ulrik Hasemann and Mathias Svold.",
-    src: "house.jpg",
+    src: "3.jpg",
     link: "https://www.ignant.com/2019/03/13/a-photographic-series-depicting-the-uncertain-future-of-denmarks-treasured-coastlines/",
     color: "#B62429"
   },
   {
     title: "Mark Rammers",
     description: "Dutch photographer Mark Rammers has shared with IGNANT the first chapter of his latest photographic project.",
-    src: "cactus.jpg",
+    src: "4.jpg",
     link: "https://www.ignant.com/2023/04/12/mark-rammers-all-over-again-is-a-study-of-regret-and-the-willingness-to-move-forward/",
     color: "#88A28D"
   }
