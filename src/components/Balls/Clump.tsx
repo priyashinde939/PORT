8fbcd36492eba6fc5a830dd74da7cfe89808b0d2
priya shinde <priya.shinde9390@gmail.com>
2024-10-04 09:58:11 +0530

    build(deps): update package-lock.json, Balls.js, Clump.tsx and HowItWorks.module.css

--- src/components/Balls/Clump.tsx
+++ src/components/Balls/Clump.tsx
@@ -1,16 +1,16 @@
-"use client";
+// "use client";
 
-import { useThree } from "@react-three/fiber";
-// import { Group } from "three";
-import  Balls  from "../Balls/Balls";
-import { Suspense } from "react";
+// import { useThree } from "@react-three/fiber";
+// // import { Group } from "three";
+// import  Balls  from "../Balls/Balls";
+// import { Suspense } from "react";
 
-export default function Clump() {
-  return (
-    <group>
-      <Suspense fallback={null}>
-        <Balls />
-      </Suspense>
-    </group>
-  );
-}
+// export default function Clump() {
+//   return (
+//     <group>
+//       <Suspense fallback={null}>
+//         <Balls />
+//       </Suspense>
+//     </group>
+//   );
+// }
