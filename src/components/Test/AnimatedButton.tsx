363c9c93ba174da236b96c247ac7a48cc76a95ea
priya shinde <priya.shinde9390@gmail.com>
2024-12-29 00:33:29 +0530

    update AnimatedButton.tsx

--- src/components/Test/AnimatedButton.tsx
+++ src/components/Test/AnimatedButton.tsx
@@ -79,6 +79,7 @@ export const FlipButton: React.FC<FlipButtonProps> = ({
           >
             {char}
           </motion.span>
+          
         ))}
       </div>
       <div className="absolute inset-0">
